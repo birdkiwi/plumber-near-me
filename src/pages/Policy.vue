03eb9aedeb4f9bc0a845a713a8f78fe24ed25bd8
alexey <alexey@toklovich.com>
2020-02-07 16:28:26 +0700

    Static pages

--- src/pages/Policy.vue
+++ src/pages/Policy.vue
@@ -1,12 +1,113 @@
 <template>
     <div class="app-route">
-        <Header />
+        <Header/>
         <main class="main-main main-main-policy">
             <div class="main-wrapper">
-                Policy
+                <div class="static-page">
+                    <h1 class="static-page-title">
+                        Privacy policy
+                    </h1>
+                    <h2>About Us</h2>
+                    <p>Fixonair (LAI, we, us) is committed to protecting your privacy. This Privacy Policy tells you how
+                        we collect, use, store, transfer and disclose your personal information. By using this site (the
+                        Site) you agree to your personal information being processed in accordance with this Privacy
+                        Policy, as amended from time to time. You should read this Privacy Policy in conjunction with
+                        our <router-link to="/terms">Terms of Use</router-link>.</p>
+                    <h2>What personal information do we collect?</h2>
+                    <p>We may collect the following personal information about you when you access our Site, including
+                        during your application for credit with LAI:</p>
+                    <ul>
+                        <li>name and contact information</li>
+                        <li>financial information</li>
+                        <li>Personal information</li>
+                    </ul>
+                    <p>We may also obtain personal information (including credit information) from credit bureaus or
+                        other lenders, or from pre-authorized debit forms, loan agreements or credit life and disability
+                        insurance applications or policies issued for you, whether at the time of your initial
+                        application or from time to time on an ongoing basis.</p>
+                    <h2>Non-personal identification information we collect</h2>
+                    <p>We may collect non-personal information about you when you interact with our Site, including
+                        computer and technical information about your means of connection to our Site. Non-personal
+                        identification information may include the browser name, operating system, Internet service
+                        provider and other similar information. We may use this information to improve our Site and
+                        provide services such as technical support. We may also provide aggregate statistics about our
+                        customers, sales, traffic patterns, and related Site information to reputable third parties, but
+                        this data will not include personally identifiable information related to specific
+                        individuals.</p>
+                    <p>How we may use your personal information</p>
+                    <p>We may collect and use your personal information either internally in connection with our
+                        business or to provide services to you, or through carefully vetted third party service
+                        providers, for the following purposes:</p>
+                    <ol>
+                        <li>confirmation of your identity;</li>
+                        <li>the initial and ongoing credit assessment of your credit worthiness, employment, and
+                            application information;</li>
+                        <li>to meet the legal and regulatory requirements applicable to us, our service providers and in
+                            connection with any loans provided to you;</li>
+                        <li>creation of a loan, credit life and disability insurance coverage, or electronic
+                            pre-authorized debits of your bank account for loan payments;</li>
+                        <li>administration of your loan(s) and collection of payments from you before or after a
+                            default;</li>
+                        <li>marketing of our services to you, as well as those of our affiliates and third parties;
+                            and</li>
+                        <li>statistical analysis for our business plans and to raise capital or loans for our business,
+                            including sales of portfolios of loans to third parties.</li>
+                    </ol>
+                    <h2>Anti-spam</h2>
+                    <p>Where applicable, we will seek your express consent to send you information, including by way of
+                        commercial electronic messages. This consent is sought by Fixonair at 4773 Yonge St, North
+                        York, ON M2N 5M5 You can unsubscribe at any time by following the instructions in the commercial
+                        electronic message, or by contacting us at {{ phone }}.</p>
+                    <p>Please be aware that, even if you have opted out of receiving marketing communications from us,
+                        we may still contact you for other purposes. For example, we may contact you to provide other
+                        newsletters or other communications you have consented to receive, regarding your loan, or if
+                        you contact us with an inquiry.</p>
+                    <h2>Transfers and disclosures of personal information</h2>
+                    <p>We will transfer or disclose your personal information in limited circumstances, including the
+                        financial institution processing your pre-authorized debt payments, the insurance company
+                        providing your credit life and accidental disability policy to us, credit reporting agencies,
+                        third parties to whom you direct any loan to be paid, our lenders and other service providers
+                        that assist us to provide and administer loans, our affiliated companies and its lenders, and to
+                        any third parties that may purchase your loan from us. We may also disclose your personal
+                        information with your consent, when required or permitted to by law, or in the event of a
+                        corporate amalgamation, reorganization, change of control or sale.</p>
+                    <h2>Security and retention</h2>
+                    <p>We protect the personal information within our control with appropriate organizational,
+                        technological and physical safeguards. We understand that some of the personal information we
+                        hold may be sensitive information, including financial and credit information, and employ
+                        appropriately robust safeguards. We store personal information in electronic and physical files
+                        that are secure, and our security measures include secure on- and off-site storage, restricted
+                        access to records and data processing equipment, password protocols, and encryption and security
+                        software.</p>
+                    <p>The personal information that we hold is only retained for as long as is necessary for the
+                        purpose for which it was collected. When we no longer are required to retain the information, we
+                        will destroy, erase, or de-personalize the information. Legal requirements, however, may require
+                        us to retain some or all of the personal information we hold for a period of time that is longer
+                        than that for which we might otherwise hold.</p>
+                    <p>Your personal information may be stored, processed or otherwise used by us, our affiliates or our
+                        service providers, both inside and outside of Canada. As a result, that country's courts,
+                        governments or law enforcement agencies could obtain disclosure of your information in
+                        accordance with that country's laws.</p>
+                    <h2>Cookies</h2>
+                    <p>We use cookies and pixel tags to track users of our Site. Cookies are pieces of information
+                        stored on a user's computer by a website so preferences are remembered on future requests. Pixel
+                        tags (also known as Web Bugs, Web Beacons, Clear Gifts, Invisible Gifts, and Tracker Gifts) are
+                        signals sent from certain websites or e-mail used to pass certain information to our servers to
+                        personalize our web sites and to track your usage. We do not save password or credit card number
+                        information on cookies or through the use of pixel tags without your permission. Rather, cookies
+                        and pixel tags are used to optimize the performance and functionality of the Site, and to
+                        personalize the Site for you and present you with content and offers specific to your
+                        interests.</p>
+                    <p>If you are uncomfortable with the idea of your information being stored in cookies, we recommend
+                        disabling cookie support in your browser. However, please note that this may limit the Site’s
+                        performance functionality.</p>
+                    <h2>Your rights</h2>
+                    <p>If you wish to see your file or make corrections to it, write to the Privacy Officer at our
+                        offices at Fixonair, 4773 Yonge St, North York, ON M2N 5M5</p>
+                </div>
             </div>
         </main>
-        <Footer />
+        <Footer/>
     </div>
 </template>
 <script>
--- src/pages/Terms.vue
+++ src/pages/Terms.vue
@@ -1,15 +1,103 @@
 <template>
     <div class="app-route">
-        <Header />
+        <Header/>
         <main class="main-main main-main-terms">
             <div class="main-wrapper">
-                Terms
+                <div class="static-page">
+                    <h1 class="static-page-title">
+                        Terms and conditions
+                    </h1>
+                    <p>The Fixonair (we, us) website (Site) is offered to you conditioned on your acceptance without
+                        modification of the terms, conditions, and notices contained herein, as amended from time to
+                        time. Your use of the Site constitutes your agreement to all such terms, conditions, and
+                        notices. Site may only be accessed by those who are 13 years of age or older.</p>
+                    <h2>No unlawful or prohibited use</h2>
+                    <p>As a condition of your use of the Site, you warrant to us that you will not use the Site for
+                        any purpose that is unlawful or prohibited by these Terms of Use. You may not use the Site
+                        in any manner which could damage, disable, overburden, or impair the Site or interfere with
+                        any other party's use and enjoyment of the Site. You may not obtain or attempt to obtain any
+                        materials or information through any means not intentionally made available or provided for
+                        through the Site.</p>
+                    <p>Subject to your compliance with these Terms of Use, we and our content providers grant you a
+                        limited, non-exclusive, non-transferable, non-assignable license to access and make personal
+                        and non-commercial use of the website. This license does not include any resale or
+                        commercial use of the Site, or its contents; any downloading or copying of account
+                        information for the benefit of another party; or any use of data mining, robots, or similar
+                        data gathering and extraction tools. All rights not expressly granted to you in these Terms
+                        of Use are reserved and retained by us and our licensors, affiliates, suppliers, publishers,
+                        rights holders, or other content providers. No portion of the Site may be reproduced,
+                        duplicated, copied, sold, resold, visited, or otherwise exploited for any commercial purpose
+                        without our express written consent. You may not frame or utilize framing techniques to
+                        enclose any of our trademarks, logos, or other proprietary information (including images,
+                        text, page layout, or form) without express written consent. You may not use any meta tags
+                        or any other "hidden text" utilizing our name or trademarks without our express written
+                        consent. You may not misuse the Site. You may use the Site only as permitted by law. The
+                        licenses we grant herein will terminate if you do not comply with these Terms of Use.</p>
+                    <h2>Termination/access restrictions</h2>
+                    <p>We reserve the right, in our sole discretion, to terminate your access to the Site or any
+                        portion thereof, and any related services available on the Site at any time, without
+                        notice.</p>
+                    <h2>Privacy</h2>
+                    <p>Any personal information that we collect on the Site will be handled in accordance with our
+                        <router-link to="/policy">Privacy Policy</router-link>.</p>
+                    <h2>Copyright and trademark notices</h2>
+                    <p>All contents of the Site are: Copyright 2020 by Fixonair and/or our affiliates. The names of
+                        actual companies and products mentioned herein may be the trademarks of their respective
+                        owners. Any rights not expressly granted herein are reserved.</p>
+                    <h2>Liability disclaimer</h2>
+                    <p>THE INFORMATION, PRODUCTS, AND SERVICES INCLUDED IN OR AVAILABLE THROUGH THE SITE MAY INCLUDE
+                        INACCURACIES OR TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY ADDED TO THE INFORMATION
+                        HEREIN. WE AND/OR OUR AFFILIATES MAY MAKE IMPROVEMENTS AND/OR CHANGES IN THE SITE AT ANY
+                        TIME.</p>
+                    <p>WE AND/OR OUR AFFILIATES MAKE NO REPRESENTATIONS ABOUT THE SUITABILITY, RELIABILITY,
+                        AVAILABILITY, TIMELINESS, CURRENCY AND ACCURACY OF THE INFORMATION CONTAINED ON THE SITE FOR
+                        ANY PURPOSE. TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, ALL SUCH INFORMATION AND THE
+                        PRODUCTS AND SERVICES ARE PROVIDED "AS IS" WITHOUT WARRANTY OR CONDITION OF ANY KIND. WE AND
+                        OUR AFFILIATES HEREBY DISCLAIM ALL WARRANTIES AND CONDITIONS WITH REGARD TO THIS
+                        INFORMATION, PRODUCTS, SERVICES, INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF TITLE,
+                        NON-INFRINGEMENT, AND OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE. WE RESERVE THE
+                        RIGHT TO MODIFY AND/OR REMOVE SUCH INFORMATION AT OUR SOLE DISCRETION WITH NO FURTHER
+                        NOTICE.</p>
+                    <p>TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL WE OR OUR AFFILIATES BE
+                        LIABLE FOR ANY DIRECT, INDIRECT, PUNITIVE, INCIDENTAL, SPECIAL, CONSEQUENTIAL DAMAGES OR ANY
+                        DAMAGES WHATSOEVER INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF USE, DATA OR PROFITS,
+                        ARISING OUT OF OR IN ANY WAY CONNECTED WITH THE USE OR PERFORMANCE OF THE SITE, WITH THE
+                        DELAY OR INABILITY TO USE THE SITE OR RELATED SERVICES, THE PROVISION OF OR FAILURE TO
+                        PROVIDE SERVICES, OR OTHERWISE ARISING OUT OF THE USE OF THE SITE, WHETHER BASED ON
+                        CONTRACT, TORT, NEGLIGENCE, STRICT LIABILITY OR OTHERWISE, EVEN IF WE OR ANY OF OUR
+                        AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF DAMAGES. BECAUSE SOME JURISDICTIONS DO
+                        NOT ALLOW THE EXCLUSION OR LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES,
+                        THE ABOVE LIMITATION MAY NOT APPLY TO YOU. IF YOU ARE DISSATISFIED WITH ANY PORTION OF THE
+                        SITE, OR WITH ANY OF THESE TERMS OF USE, YOUR SOLE AND EXCLUSIVE REMEDY IS TO DISCONTINUE
+                        USING THE SITE.</p>
+                    <h2>Links to third party sites</h2>
+                    <p>The Site may contain links to other websites ("Linked Sites"). The Linked Sites are not under
+                        the control of the Site and we are not responsible for the contents of any Linked Site,
+                        including without limitation any link contained in a Linked Site, or any changes or updates
+                        to a Linked Site. We are not responsible for webcasting or any other form of transmission
+                        received from any Linked Site. We are providing these links to you only as a convenience,
+                        and the inclusion of any link does not imply our endorsement or any association with its
+                        operators.</p>
+                    <h2>Idea submission statement</h2>
+                    <p>Any ideas, proposals, and/or other information submitted or collected on the Site (excluding
+                        information provided pursuant to an application for credit) shall be considered
+                        non-confidential. By submitting information to the Site, or directly to us, you agree that
+                        we shall not be under any obligation of confidentiality or non-use, express or implied, with
+                        respect to any submissions, and we may disclose or use any submission for any purpose
+                        whatsoever, at our sole discretion, with no obligation, express or implied, to compensate or
+                        work with the person or entity that supplied the information, ideas and/or proposal in any
+                        manner.</p>
+                    <h2>Service contact</h2>
+                    <p><a :href="'mailto:' + email">{{ email }}</a></p>
+                </div>
             </div>
         </main>
-        <Footer />
+        <Footer/>
     </div>
 </template>
 <script>
+    import config from '../config';
+    import '../assets/css/pages/static.css';
     import Header from '../components/Header.vue';
     import Footer from '../components/Footer.vue';
 
@@ -17,6 +105,11 @@
         components: {
             Header,
             Footer,
+        },
+        data() {
+            return {
+                email: config.contactEmail
+            }
         }
     }
 </script>
